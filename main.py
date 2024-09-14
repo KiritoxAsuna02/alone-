83e635f09379ecb7c32d7f911dde86b070bd7531
KiritoxAsuna02 <133078081+KiritoxAsuna02@users.noreply.github.com>
2024-09-14 15:09:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ from core import (
 REPO = """
 🤖 **Music Player**
 
-- Repo: [GitHub](https://github.com/AsmSafone/MusicPlayer)
+- Repo: [GitHub](https://github.com/KiritoxAsuna02/alone-)
 - License: AGPL-3.0-or-later
 """
 
